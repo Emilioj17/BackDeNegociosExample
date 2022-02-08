24ff7000ede8ecd093494d42dd9cadb1dffc95a6
Emilioj17 <ejjelves@gmail.com>
2022-02-08 19:56:03 -0300

    Ok

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def login_usuario():
     usuario = Usuario.query.filter_by(correo=correo).first()
     '''if usuario is not None and check_password_hash(usuario.clave, clave) == True: '''
     if usuario is not None:
-        if check_password_hash(usuario.clave, clave) == True:
+        if check_password_hash(str(usuario.clave), clave) == True:
             token = create_access_token(identity=clave)
             return jsonify(usuario.serialize(), token), 200
     else:
