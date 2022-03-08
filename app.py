56aff167afb6563efab0f0bbb323f7c5255f02cb
Emilioj17 <ejjelves@gmail.com>
2022-03-08 16:33:50 -0300

    Ok

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from models import db, Usuario, ClienteDt, Nota, Dt2019, Dt2020, Dt2021, Dt2022, Dt2023, Dt2024
+from models import ClienteContabilidad, PagosContabilidad, NotaContabilidad
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token, JWTManager
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -11,11 +12,11 @@ from flask import render_template
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://admin:curso2021@database-1.clcxl18xumje.us-east-1.rds.amazonaws.com/contabilidad"
 ''' os.environ.get('DB_CONNECTION_STRING') mysql+mysqlconnector://admin:curso2021@database-1.clcxl18xumje.us-east-1.rds.amazonaws.com/basedatos '''
 db.init_app(app)
 Migrate(app, db)
-app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET_KEY')
+app.config["JWT_SECRET_KEY"] = "@alfa123@254alfacentaurizxcKKvbnm@123456789ASDFGHJKL"
 '''os.environ.get('JWT_SECRET_KEY') @alfa123@254alfacentaurizxcKKvbnm@123456789ASDFGHJKL'''
 jwt = JWTManager(app)
 
@@ -102,6 +103,9 @@ def usuarios(id=None):
         usuario.delete()
         return jsonify({"success": "User deleted"}), 200
 
+''' DIRECCION TRIBUTARIA '''
+
+
 ''' Esta es la ruta que genera los clientes en la Vista Ampliada de Direccion Tributaria '''
 @app.route('/xDt/<int:page_num>', methods=['GET'])
 def xDt(page_num=None):
@@ -134,7 +138,6 @@ def busquedaDt():
 ''' Esta es la Ruta para hacer Filtros en Vista Ampliada '''
 @app.route('/filtroDt', methods=['POST'])  
 def filtroDt():
-    print("Hola")
     request_body = request.data
     print(request_body)
     decoded_object = json.loads(request_body)
@@ -165,9 +168,6 @@ def filtroDt():
     if tipoPago != None:
         clientesDt = ClienteDt.query.filter(ClienteDt.tipoPago.like(tipoPago)).all()
 
-    ''' print(filtrado) '''
-    ''' filtrado = "%{}%".format(filtrado) '''
-    ''' clientesDt = ClienteDt.query.filter(ClienteDt.vigente.like(filtrado)).all() '''
     paginas = 1
     pagina = 1
     if clientesDt is not None:
@@ -816,6 +816,268 @@ def Notas(id=None):
 
         return jsonify(nota.serialize()), 201
 
+''' CONTABILIDAD '''
+
+''' Esta es la ruta que genera los clientes en la Vista Ampliada de Contabilidad '''
+@app.route('/xContabilidad/<int:page_num>', methods=['GET'])
+def xContabilidad(page_num=None):
+    if (request.method == 'GET'):
+        clientesContabilidad= ClienteContabilidad.query.paginate(per_page=1000, page=page_num, error_out=True)
+        paginas = clientesContabilidad.pages
+        pagina = clientesContabilidad.page
+        clientesContabilidad= clientesContabilidad.items
+        clientesContabilidad = list(
+            map(lambda clienteContabilidad: clienteContabilidad.serialize(), clientesContabilidad))
+        return jsonify(clientesContabilidad, paginas, pagina), 200
+
+''' Esta es la ruta de Busqueda para la Barra de Busqueda. '''
+@app.route('/busquedaContabilidad', methods=['POST'])  
+def busquedaContabilidad():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    busqueda = decoded_object["busqueda"]
+    busqueda = "%{}%".format(busqueda)
+    clientesContabilidad = ClienteContabilidad.query.filter((ClienteContabilidad.razon.like(busqueda)) | (ClienteContabilidad.rut.like(busqueda)) | (ClienteContabilidad.correo.like(busqueda)) | (ClienteContabilidad.correoSecundario.like(busqueda)) | (ClienteContabilidad.correoTerciario.like(busqueda)) | (ClienteContabilidad.fono.like(busqueda)) | (ClienteContabilidad.id.like(busqueda)) | (ClienteContabilidad.libre.like(busqueda))).all()
+    paginas = 1
+    pagina = 1
+    if clientesContabilidad is not None:
+        clientesContabilidad = list(
+            map(lambda clienteContabilidad: clienteContabilidad.serializeInfo(), clientesContabilidad))
+        return jsonify(clientesContabilidad, paginas, pagina), 200
+    else:
+        return jsonify({"Error": "Tu busqueda no ha Arrojado Resultados"}), 401
+
+''' Esta es la Ruta para hacer Filtros en Vista Ampliada '''
+@app.route('/filtroContabilidad', methods=['POST'])  
+def filtroContabilidad():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    vigente = decoded_object['vigente']
+    whatsapp = decoded_object['whatsapp']
+    erpyme = decoded_object['erpyme']
+    dicom = decoded_object['dicom']
+    repetido = decoded_object['repetido']
+
+    if vigente != None:
+        clientesContabilidad = ClienteContabilidad.query.filter(ClienteContabilidad.vigente.like(vigente)).all()
+    if whatsapp != None:
+        clientesContabilidad = ClienteContabilidad.query.filter(ClienteContabilidad.whatsapp.like(whatsapp)).all()
+    if erpyme != None:
+        clientesContabilidad = ClienteContabilidad.query.filter(ClienteContabilidad.erpyme.like(erpyme)).all()
+    if dicom != None:
+        clientesContabilidad = ClienteContabilidad.query.filter(ClienteContabilidad.dicom.like(dicom)).all()
+    if repetido != None:
+        clientesContabilidad = ClienteContabilidad.query.filter(ClienteContabilidad.repetido.like(repetido)).all()
+
+    paginas = 1
+    pagina = 1
+    if clientesContabilidad is not None:
+        clientesContabilidad = list(
+            map(lambda clienteDt: clienteDt.serializeInfo(), clientesContabilidad))
+        return jsonify(clientesContabilidad, paginas, pagina), 200
+    else:
+        return jsonify({"Error": "Tu filtrado no ha Arrojado Resultados"}), 401
+
+@app.route('/clienteContabilidad', methods=['POST'])
+@app.route('/clienteContabilidad/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def clientesContabilidad(id=None):
+    if (request.method == 'GET'):
+        if(id is not None):
+            clienteContabilidad = ClienteContabilidad.query.get(id)
+            return jsonify(clienteContabilidad.serializeInfo()), 200
+
+    if (request.method == 'POST'):
+        request_body = request.data
+        decoded_object = json.loads(request_body)
+        razon = decoded_object['razon']
+        rut = decoded_object['rut']
+        vigente = decoded_object['vigente']
+        correo = decoded_object['correo']
+        correoSecundario = decoded_object['correoSecundario']
+        correoTerciario = decoded_object['correoTerciario']
+        fono = decoded_object['fono']
+        whatsapp = decoded_object['whatsapp']
+        erpyme = decoded_object['erpyme']
+        dicom = decoded_object['dicom']
+        repetido = decoded_object['repetido']
+        libre = decoded_object['libre']
+
+        clienteContabilidad = ClienteContabilidad()
+        clienteContabilidad.razon = razon
+        clienteContabilidad.rut = rut
+        clienteContabilidad.vigente = vigente
+        clienteContabilidad.correo = correo
+        clienteContabilidad.correoSecundario = correoSecundario
+        clienteContabilidad.correoTerciario = correoTerciario
+        clienteContabilidad.fono = fono
+        clienteContabilidad.whatsapp = whatsapp
+        clienteContabilidad.erpyme = erpyme
+        clienteContabilidad.dicom = dicom
+        clienteContabilidad.repetido = repetido
+        clienteContabilidad.libre = libre
+
+        clienteContabilidad.save()
+
+        return jsonify(clienteContabilidad.serialize()), 201
+
+    if (request.method == 'PUT'):
+        request_body = request.data
+        decoded_object = json.loads(request_body)
+        razon = decoded_object['razon']
+        rut = decoded_object['rut']
+        vigente = decoded_object['vigente']
+        correo = decoded_object['correo']
+        correoSecundario = decoded_object['correoSecundario']
+        correoTerciario = decoded_object['correoTerciario']
+        fono = decoded_object['fono']
+        whatsapp = decoded_object['whatsapp']
+        erpyme = decoded_object['erpyme']
+        dicom = decoded_object['dicom']
+        repetido = decoded_object['repetido']
+        libre = decoded_object['libre']
+
+        clienteContabilidad = ClienteContabilidad.query.get(id)
+        if razon != None:
+            clienteContabilidad.razon = razon
+        if rut != None:
+            clienteContabilidad.rut = rut
+        if vigente != None:
+            clienteContabilidad.vigente = vigente
+        if correo != None:
+            clienteContabilidad.correo = correo
+        if correoSecundario != None:
+            clienteContabilidad.correoSecundario = correoSecundario
+        if correoTerciario != None:
+            clienteContabilidad.correoTerciario = correoTerciario
+        if fono != None:
+            clienteContabilidad.fono = fono
+        if whatsapp != None:
+            clienteContabilidad.whatsapp = whatsapp
+        if erpyme != None:
+            clienteContabilidad.erpyme = erpyme
+        if dicom != None:
+            clienteContabilidad.dicom = dicom
+        if repetido != None:
+            clienteContabilidad.repetido = repetido
+        if libre != None:
+            clienteContabilidad.libre = libre
+
+        clienteContabilidad.update()
+
+        return jsonify(clienteContabilidad.serialize()), 201
+
+    if (request.method == 'DELETE'):
+        clienteContabilidad = ClienteContabilidad.query.get(id)
+        clienteContabilidad.delete()
+        return jsonify({"success": "Deal deleted"}), 200
+
+
+@app.route('/pagosContabilidad', methods=['GET', 'POST'])
+@app.route('/pagosContabilidad/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def pagosContabilidad(id=None):
+    if (request.method == 'GET'):
+        if(id is not None):
+            detallesPago = PagosContabilidad.query.filter_by(clienteContabilidadid=id).all()
+            detallesPago = list(
+                map(lambda detallePago: detallePago.serialize(), detallesPago))
+            return jsonify(detallesPago), 200
+        detallesPago = PagosContabilidad.query.all()
+        detallesPago = list(
+            map(lambda detallePago: detallePago.serialize(), detallesPago))
+        return jsonify(detallesPago), 200
+
+    if (request.method == 'POST'):
+        request_body = request.data
+        decoded_object = json.loads(request_body)
+        year = decoded_object['year']
+        mes = decoded_object['mes']
+        numeroTransferencia = decoded_object['numeroTransferencia']
+        montoPagado = decoded_object['montoPagado']
+        montoCobrado = decoded_object['montoCobrado']
+        facturaNumero = decoded_object['facturaNumero']
+        comentario = decoded_object['comentario']
+        clienteContabilidadid = decoded_object['clienteContabilidadid']
+
+        pagoContabilidad = PagosContabilidad()
+        pagoContabilidad.year = year
+        pagoContabilidad.mes = mes
+        pagoContabilidad.numeroTransferencia = numeroTransferencia
+        pagoContabilidad.montoPagado = montoPagado
+        pagoContabilidad.montoCobrado = montoCobrado
+        pagoContabilidad.facturaNumero = facturaNumero
+        pagoContabilidad.comentario = comentario
+        pagoContabilidad.clienteContabilidadid = clienteContabilidadid
+
+        pagoContabilidad.save()
+
+        return jsonify(pagoContabilidad.serialize()), 201
+
+    if (request.method == 'PUT'):
+        request_body = request.data
+        decoded_object = json.loads(request_body)
+        year = decoded_object['year']
+        mes = decoded_object['mes']
+        numeroTransferencia = decoded_object['numeroTransferencia']
+        montoPagado = decoded_object['montoPagado']
+        montoCobrado = decoded_object['montoCobrado']
+        facturaNumero = decoded_object['facturaNumero']
+        comentario = decoded_object['comentario']
+
+        pagoContabilidad = PagosContabilidad.query.get(id)
+
+        if year != None:
+            pagoContabilidad.year = year
+        if mes != None:
+            pagoContabilidad.mes = mes
+        if numeroTransferencia != None:
+            pagoContabilidad.numeroTransferencia = numeroTransferencia
+        if montoPagado != None:
+            pagoContabilidad.montoPagado = montoPagado
+        if montoCobrado != None:
+            pagoContabilidad.montoCobrado = montoCobrado
+        if comentario != None:
+            pagoContabilidad.comentario = comentario
+        if facturaNumero != None:
+            pagoContabilidad.facturaNumero = facturaNumero
+
+        pagoContabilidad.update()
+
+        return jsonify(pagoContabilidad.serialize()), 201
+
+    if (request.method == 'DELETE'):
+        pagoContabilidad = PagosContabilidad.query.get(id)
+        pagoContabilidad.delete()
+        return jsonify({"success": "Deal deleted"}), 200
+
+@app.route('/notaContabilidad', methods=['GET', 'POST'])
+@app.route('/notaContabilidad/<int:id>', methods=['GET'])
+def NotasContabilidad(id=None):
+    if (request.method == 'GET'):
+        if(id is not None):
+            notas = NotaContabilidad.query.filter_by(clienteDtid=id).all()
+            notas = list(
+                map(lambda nota: nota.serialize(), notas))
+            return jsonify(notas), 200
+        notas = NotaContabilidad.query.all()
+        notas = list(
+            map(lambda nota: nota.serialize(), notas))
+        return jsonify(notas), 200
+
+    if (request.method == 'POST'):
+        request_body = request.data
+        decoded_object = json.loads(request_body)
+        comentario = decoded_object['comentario']
+        fechaComentario = decoded_object['fechaComentario']
+        clienteContabilidadid = decoded_object['clienteContabilidadid']
+
+        nota = NotaContabilidad()
+        nota.comentario = comentario
+        nota.fechaComentario = fechaComentario
+        nota.clienteContabilidadid = clienteContabilidadid
+
+        nota.save()
+
+        return jsonify(nota.serialize()), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -32,6 +32,7 @@ class Usuario(db.Model):
         db.session.delete(self)
         db.session.commit()
 
+''' DIRECCION TRIBUTARIA '''
 
 class ClienteDt(db.Model):
     __tablename__ = 'clientesDt'
@@ -456,3 +457,151 @@ class Nota(db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+
+
+''' CONTABILIDAD '''
+
+class ClienteContabilidad(db.Model):
+    __tablename__ = 'clientesContabilidad'
+    id = db.Column(db.Integer, primary_key=True)
+    razon = db.Column(db.String(100), nullable=False)
+    rut = db.Column(db.String(20), nullable=False)
+    vigente = db.Column(db.String(10), nullable=True, default="Si")
+    correo = db.Column(db.String(50), nullable=False)
+    correoSecundario = db.Column(db.String(50), nullable=True)
+    correoTerciario = db.Column(db.String(50), nullable=True)
+    fono = db.Column(db.String(10), nullable=True)
+    whatsapp = db.Column(db.String(10), nullable=True)
+    erpyme = db.Column(db.String(10), nullable=True, default="No")
+    dicom = db.Column(db.String(10), nullable=True, default="No")
+    repetido = db.Column(db.String(10), nullable=True, default="No")
+    libre = db.Column(db.String(200), nullable=True)
+    pagosContabilidadID = db.relationship(
+        'PagosContabilidad', cascade='all, delete', backref='pagosContabilidad')
+    notasContabilidadID = db.relationship(
+        'NotaContabilidad', cascade='all, delete', backref='clientesContabilidadNotas')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "razon": self.razon,
+            "rut": self.rut,
+            "vigente": self.vigente,
+            "correo": self.correo,
+            "correoSecundario": self.correoSecundario,
+            "correoTerciario": self.correoTerciario,
+            "fono": self.fono,
+            "whatsapp": self.whatsapp,
+            "erpyme": self.erpyme,
+            "dicom": self.dicom,
+            "repetido": self.repetido,
+            "libre": self.libre,
+            "pagosContabilidadID": self.get_pagosContabilidad(),
+            "notasContabilidad": self.get_notasContabilidad()
+        }
+
+    '''La siguiente funcion, muestra info especifica del cliente en la vista de detalle'''
+    def serializeInfo(self):
+        return {
+        "id": self.id,
+        "razon": self.razon,
+        "rut": self.rut,
+        "vigente": self.vigente,
+        "correo": self.correo,
+        "correoSecundario": self.correoSecundario,
+        "correoTerciario": self.correoTerciario,
+        "fono": self.fono,
+        "whatsapp": self.whatsapp,
+        "erpyme": self.erpyme,
+        "dicom": self.dicom,
+        "repetido": self.repetido,
+        "libre": self.libre
+    }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def get_pagosContabilidad(self):
+        pagos = list(map(lambda pago: pago.serialize(),
+                     self.pagosContabilidadID))  # Revisar aqui, es probable que tenga malo el lambda.
+        return pagos
+
+    def get_notasContabilidad(self):
+        notas = list(map(lambda nota: nota.serialize(),
+                         self.notasContabilidadID))  # Revisar aqui, es probable que tenga malo el lambda.
+        return notas
+
+
+class PagosContabilidad(db.Model):
+    __tablename__ = 'pagosContabilidad'
+    id = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.String(10), nullable=False)
+    mes = db.Column(db.String(10), nullable=False)
+    numeroTransferencia = db.Column(db.String(100), nullable=True)
+    montoPagado = db.Column(db.String(50), nullable=True)
+    montoCobrado = db.Column(db.String(100), nullable=False, default="9900")
+    facturaNumero = db.Column(db.String(30), nullable=True)
+    comentario = db.Column(db.String(100), nullable=True)
+    clienteContabilidadid = db.Column(db.Integer, db.ForeignKey(
+        'clientesContabilidad.id', ondelete='CASCADE'))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "year": self.year,
+            "mes": self.mes,
+            "numeroTransferencia": self.numeroTransferencia,
+            "montoPagado": self.montoPagado,
+            "montoCobrado": self.montoCobrado,
+            "facturaNumero": self.facturaNumero,
+            "comentario": self.comentario,
+            "clienteContabilidadid": self.clienteContabilidadid
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class NotaContabilidad(db.Model):
+    __tablename__ = 'notasContabilidad'
+    id = db.Column(db.Integer, primary_key=True)
+    comentario = db.Column(db.String(500), nullable=False)
+    fechaComentario = db.Column(db.String(20), nullable=False)
+    clienteContabilidadid = db.Column(db.Integer, db.ForeignKey(
+        'clientesContabilidad.id', ondelete='CASCADE'))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comentario": self.comentario,
+            "fechaComentario": self.fechaComentario,
+            "clienteContabilidadid": self.clienteContabilidadid
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
